86e358ee4b92c902960bd8e662a166fb3509e061
fez <3151783107@qq.com>
2024-03-19 16:28:51 +0800

    Header completed

---
+++ ai-prompt-market/app/(Main)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '@/components/Header'
+type Props = {}
+
+const Page = (props: Props) => {
+  return (
+    <div>
+      <Header activeItem={0} />
+    </div>
+  )
+}
+
+export default Page
